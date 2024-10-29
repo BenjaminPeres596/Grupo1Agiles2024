ae18330780c97aee07b603b0391a9ed002648d53
Benjamin Peres <benjaminperes596@gmail.com>
2024-10-29 20:05:12 -0300

    [feat]: estilizo el filtro de distancia

--- components/distancefilter.tsx
+++ components/distancefilter.tsx
@@ -1,33 +1,63 @@
-import React, { useState } from 'react';
-import { View, Text, Button } from 'react-native';
-import Slider from '@react-native-community/slider';
+import React, { useState } from "react";
+import { View, Text, StyleSheet } from "react-native";
+import Slider from "@react-native-community/slider";
 
 // Definir la interfaz para las propiedades
 interface DistanceFilterProps {
   onDistanceChange: (distance: number) => void; // Define el tipo de la función
 }
 
-const DistanceFilter: React.FC<DistanceFilterProps> = ({ onDistanceChange }) => {
-  const [maxDistance, setMaxDistance] = useState(10000); // Valor inicial en metros
+const DistanceFilter: React.FC<DistanceFilterProps> = ({
+  onDistanceChange,
+}) => {
+  const [maxDistance, setMaxDistance] = useState(10000);
 
   const handleApplyDistance = () => {
-    // Llama a la función para actualizar la distancia máxima
     onDistanceChange(maxDistance);
   };
 
   return (
-    <View style={{ padding: 20, backgroundColor: '#fff', borderRadius: 8, shadowColor: '#000', shadowOffset: { width: 0, height: 2 }, shadowOpacity: 0.2, shadowRadius: 4 }}>
-      <Text style={{ fontSize: 18, marginBottom: 10 }}>Distancia máxima: {maxDistance} metros</Text>
+    <View style={styles.verticalSliderContainer}>
+      <Text style={styles.distanceText}>{maxDistance} m</Text>
       <Slider
+        style={styles.verticalSlider}
         minimumValue={150}
         maximumValue={10000}
         step={10}
         value={maxDistance}
         onValueChange={(value) => setMaxDistance(value)}
+        onSlidingComplete={handleApplyDistance}
+        minimumTrackTintColor="#000000" // Color del track mínimo (parte que se llena)
+        maximumTrackTintColor="#000000" // Color del track máximo (parte que no se llena)
+        thumbTintColor="#000000" // Color del thumb (parte que se arrastra)
       />
-      <Button title="Aplicar" onPress={handleApplyDistance} />
     </View>
   );
 };
 
 export default DistanceFilter;
+
+const styles = StyleSheet.create({
+  verticalSliderContainer: {
+    position: "absolute",
+    bottom: 10,
+    right: -70,
+    height: 200,
+    justifyContent: "center",
+    alignItems: "center",
+    zIndex: 100, // Aumenta el valor para asegurar visibilidad
+  },
+
+  verticalSlider: {
+    transform: [{ rotate: "-90deg" }], // Rotar el slider a vertical
+    width: 200, // Ajusta el ancho para el rango de movimiento
+  },
+
+  distanceText: {
+    color: "#000000", // Color del texto de la distancia
+    fontSize: 16, // Ajusta el tamaño de la fuente según necesites
+    bottom: 85,
+    right: 5,
+    textAlign: "center", // Centrar el texto
+  },
+});
