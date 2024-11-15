aec3b5fe3a09e723d1ec96c42d6f3f706e5aa1f9
Valentin Peres <peresvalentin09@gmail.com>
2024-11-14 22:42:08 -0300

    [feat]: Mejoro diseño del header

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,73 +1,69 @@
 import React from "react";
 import { View, Text, StyleSheet, TouchableOpacity, SafeAreaView } from "react-native";
 import { Ionicons } from "@expo/vector-icons";
-import { useNavigation } from "@react-navigation/native";
+import FontAwesome6 from '@expo/vector-icons/FontAwesome6';
 
 interface HeaderProps {
-  title: string;
-  onProfilePress?: () => void; // Para manejar el click en el perfil
-  onSearchPress?: () => void; // Propiedad para manejar el evento de la búsqueda
+    title: string;
+    onProfilePress?: () => void;
+    onSearchPress?: () => void;
 }
 
 const Header: React.FC<HeaderProps> = ({
-  title,
-  onProfilePress,
-  onSearchPress,
+    title,
+    onProfilePress,
+    onSearchPress,
 }) => {
-  const navigation = useNavigation();
+    return (
+        <SafeAreaView style={styles.safeArea}>
+            <View style={styles.headerContainer}>
+                <TouchableOpacity onPress={onProfilePress} style={styles.iconContainer}>
+                    <Ionicons name="star-outline" size={28} color="white" />
+                </TouchableOpacity>
 
-  return (
-    <SafeAreaView style={styles.safeArea}>
-      <View style={styles.headerContainer}>
-        <TouchableOpacity onPress={onProfilePress} style={styles.iconContainer}>
-          <Ionicons name="star-outline" size={32} color="white" />
-        </TouchableOpacity>
+                <View style={styles.titleContainer}>
+                    <Text style={styles.title}>{title}</Text>
+                    <FontAwesome6 name="plate-wheat" size={20} color="#FFFFFF" style={styles.icon} />
+                </View>
 
-        <View style={styles.titleContainer}>
-          <Text style={styles.title}>{title}</Text>
-          <Text style={styles.subText}>Comer seguro, sin gluten</Text>
-        </View>
-
-        <TouchableOpacity
-          onPress={onSearchPress} // Abre el modal al presionar la lupa
-          style={styles.iconContainer}
-        >
-          <Ionicons name="search-outline" size={28} color="white" />
-        </TouchableOpacity>
-      </View>
-    </SafeAreaView>
-  );
+                <TouchableOpacity onPress={onSearchPress} style={styles.iconContainer}>
+                    <Ionicons name="search-outline" size={28} color="white" />
+                </TouchableOpacity>
+            </View>
+        </SafeAreaView>
+    );
 };
 
 const styles = StyleSheet.create({
-  safeArea: {
-    backgroundColor: "#EF4423",
-  },
-  headerContainer: {
-    flexDirection: "row",
-    alignItems: "center",
-    justifyContent: "space-between",
-    backgroundColor: "#EF4423",
-    height: 75,
-    paddingHorizontal: 16,
-  },
-  titleContainer: {
-    flexDirection: "column",
-    alignItems: "center",
-  },
-  title: {
-    fontSize: 20,
-    fontWeight: "bold",
-    color: "white",
-  },
-  subText: {
-    fontSize: 12,
-    color: "white",
-    marginTop: 4,
-  },
-  iconContainer: {
-    padding: 8,
-  },
+    safeArea: {
+        backgroundColor: "#EF4423",
+    },
+    headerContainer: {
+        flexDirection: "row",
+        alignItems: "center",
+        justifyContent: "space-between",
+        backgroundColor: "#EF4423",
+        height: 75,
+        paddingHorizontal: 16,
+    },
+    titleContainer: {
+        alignItems: "center",
+        justifyContent: "center",
+        marginHorizontal: 16,
+    },
+    title: {
+        fontSize: 18, // Tamaño reducido del texto
+        fontWeight: "600",
+        color: "white",
+        fontFamily: "AvenirNext-DemiBold",
+        marginBottom: 2, // Espacio entre el texto y el icono
+    },
+    icon: {
+        marginTop: 2, // Espacio entre el texto y el icono
+    },
+    iconContainer: {
+        padding: 8,
+    },
 });
 
 export default Header;
